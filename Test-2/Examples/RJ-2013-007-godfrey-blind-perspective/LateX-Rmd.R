e53e7aa746045b043b9ca2d73b2df5d5f4f064c8
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2022-04-16 12:07:32 +0530

    Fixed the copying issue the script works !

--- Test-2/Examples/RJ-2013-007-godfrey-blind-perspective/LateX-Rmd.R
+++ Test-2/Examples/RJ-2013-007-godfrey-blind-perspective/LateX-Rmd.R
@@ -14,7 +14,7 @@ Append_Markdown_Files <- function(input_file,title,bib_file){
   bibliography_string="bibliography:"
   output_string="output:"
   output_path="rjtools::rjournal_web_article"
-  dir.create("output")
+  dir.create("output",showWarnings = F)
   rmd_yml_additions= paste(yml_header,newline_string,title_string,title,newline_string,bibliography_string,bib_file,newline_string,output_string,output_path,newline_string, sep =" ")
   output_file = file(paste("output/",toString(tools::file_path_sans_ext(input_file)),".Rmd",sep=""), open="wt")
   writeLines(paste(rmd_yml_additions,""),con=output_file,useBytes = FALSE)
@@ -28,17 +28,13 @@ Copy_Other_Files<-function(){
   
   dir_list=list.dirs(recursive = FALSE)
   target_dir=basename(dir_list[grep("*_files",dir_list)])
-  dir.create(paste("output/",target_dir,sep=""))
-  file.copy(list.files(target_dir, full.names = TRUE), 
-            paste("output/",target_dir,sep=""), 
-            recursive = TRUE)
+  dir.create(paste("output/",target_dir,sep=""),showWarnings = F)
+  file.copy(list.files(target_dir, full.names = TRUE), paste("output/",target_dir,sep=""), recursive = TRUE)
   file_list=list.files(recursive = FALSE)
-  
   extensions = c("*.png", "*.jpg", "*.bib")
-  target_files = unique(grep(paste(extensions,collapse="|"), 
-                             file_list, value=TRUE))
-  file.copy(from=target_files,to = "output/", copy.mode = T, recursive=FALSE)
+  target_files = unique(grep(paste(extensions,collapse="|"), file_list, value=TRUE))
+  file.copy(target_files,to = "output/", copy.mode = T, recursive=FALSE,)
 }
 Convert_to_Markdown("RJwrapper.tex")
 Append_Markdown_Files("RJwrapper.md",title = "Statistical Software from a Blind Person’s Perspective",bib_file = "Godfrey.bib")
-Copy_Other_files()
+Copy_Other_Files()
